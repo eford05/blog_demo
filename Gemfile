c79fd06aba6ac0ca62e4c7420fe5a5bf12861735
eford05 <89802740+eford05@users.noreply.github.com>
2024-09-04 22:13:09 -0500

    Add devise to gemfile, add alerts partial, define show views action in controller and show

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem "turbo-rails"
 gem "stimulus-rails"
 gem "jbuilder"
 gem "htmlbeautifier"
+gem "devise"
 
 
 gem "tzinfo-data", platforms: %i[ windows jruby ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_04_155135) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_05_023519) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 end
